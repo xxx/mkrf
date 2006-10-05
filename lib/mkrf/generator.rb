fa0c1f5ba1c0616204ea9bf3456b6448729669e5
johnmg <johnmg@6e48df83-f314-0410-b1e7-e69642b30866>
2006-10-05 02:06:21 +0000

    Tweaked the docs a little bit.
    git-svn-id: http://mkrf.rubyforge.org/svn/trunk@36 6e48df83-f314-0410-b1e7-e69642b30866

--- lib/mkrf/generator.rb
+++ lib/mkrf/generator.rb
@@ -6,9 +6,15 @@ require 'rake/tasklib'
 module Mkrf
   
   # +Generator+ is concerned with taking configuration for an extension
-  # and writing a +Rakefile+ to the local filesystem to build the extension.
+  # and writing a +Rakefile+ to the local filesystem which will later be
+  # used to build the extension.
+  #
+  # You will typically only create one +Generator+ per <tt>extconf.rb</tt>
+  # file, which in turn will generate a Rakefile for building one extension
+  # module.
   #
   # = Usage
+  #
   # In the most basic usage, +Generator+ simply takes the name of the library
   # to compile:
   #
@@ -18,7 +24,7 @@ module Mkrf
   # Configuration of the build can be passed to the +Generator+ constructor
   # as a block:
   #
-  #   Mkrf::Generator.new('libxml', 'my_extensions_dir/*.c') do |g|
+  #   Mkrf::Generator.new('libxml') do |g|
   #     g.include_library('socket','socket')
   #     g.include_header('libxml/xmlversion.h',
   #                      '/opt/include/libxml2',
@@ -35,26 +41,28 @@ module Mkrf
     attr_accessor :additional_code
     
     # You may append to these attributes directly in your Generator.new block,
-    # for example: <tt>g.object_files << ' ../common/foo.o ../common/bar.so -lmystuff'</tt> or
+    # for example: <tt>g.object << ' ../common/foo.o ../common/bar.so -lmystuff'</tt> or
     # <tt>g.cflags << ' -ansi -Wall'</tt>
     #
     # Note the extra space at the beginning of those strings.
     attr_accessor :cflags
 
-    # objects is for adding additional linked object files. Source objects will still be automatic.
+    # +objects+ is for adding _additional_ object files to the link-edit command -- outside
+    # of the ones that correspond to the source files.
     attr_accessor :objects
 
-    # Your system-specific linker command to build a shared library.
+    # Any additional options you'd like appended to your system-specific linker command
+    # (which is used to build the shared library).
     attr_accessor :ldshared
 
     
     
-    # Create a new generator which will write a new +Rakefile+ to the local
+    # Create a +Generator+ object which writes a Rakefile to the current directory of the local
     # filesystem.
     #
     # Params:
     # * +extension_name+ -- the name of the extension
-    # * +source_patterns+ -- an array of patterns describing source files to be compiled, ["*.c"] by default
+    # * +source_patterns+ -- an array of patterns describing source files to be compiled. ["*.c"] is the default.
     def initialize(extension_name, source_patterns = ["*.c"], availability_options = {})
       @sources = source_patterns
       @extension_name = extension_name + ".#{CONFIG['DLEXT']}"
